ef08e4bd1bf06808cc5c218f8e11af0c57d56503
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-07-16 22:31:06 +0900

    敵との距離を取得

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -1,17 +1,38 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Bot : MonoBehaviour{
     
     public Transform target;
-    private NavMeshAgent myAgent; 
+    private NavMeshAgent myAgent;
+
+    public GameObject bullet;
+    public float range;
+
+    private float initSpeed;
     
     void Start(){
         myAgent = GetComponent<NavMeshAgent>();
+        initSpeed = myAgent.speed;
     }
 
     void Update(){
         myAgent.SetDestination(target.position);
+        distanceCheck();
+    }
+
+    void distanceCheck(){
+        if(getDistanceToEnemy() < range){
+            Debug.Log("distance");
+            myAgent.speed = 0;
+        }else{
+            myAgent.speed = initSpeed;
+        }
+    }
+
+    float getDistanceToEnemy(){
+        return Vector3.Distance(this.transform.position,target.position);
     }
 
 }
